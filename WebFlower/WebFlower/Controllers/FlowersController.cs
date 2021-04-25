0e4649280764135f0820fbb3d5e83e017ef41aeb
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-04-25 23:11:12 +0300

    start flowers

---
+++ WebFlower/WebFlower/Controllers/FlowersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFlower.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlowersController : ControllerBase
+    {
+        public IActionResult GetResults()
+        {
+            return Ok(new { Name = "Rose", Family = "Rosaceae", Weight = 55 });
+        }
+    }
+}
